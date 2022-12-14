c74f25c965aa75287ea101f57d9a166b4fedd979
Anllelo Josue Yactayo Paz <anllelojosue.y@gmail.com>
2022-12-13 22:56:47 -0500

    EF_Anllelo_Yactayo_Config

---
+++ src/main/java/com/Anllelo/Yactayo/Paz/EfAnlleloYactayoConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.Anllelo.Yactayo.Paz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfAnlleloYactayoConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfAnlleloYactayoConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Anllelo/Yactayo/Paz/EfAnlleloYactayoConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Anllelo.Yactayo.Paz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfAnlleloYactayoConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
